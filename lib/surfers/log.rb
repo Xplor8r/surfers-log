0c0fd01726cdd65f91ff9f443cafe191734c553d
Michael Pieschel <sixtyhectares@yahoo.com>
2018-10-18 20:40:04 +0000

    first commit

---
+++ lib/surfers/log.rb
@@ -0,0 +1,7 @@
+require "surfers/log/version"
+
+module Surfers
+  module Log
+    # Your code goes here...
+  end
+end
---
+++ lib/surfers/log/version.rb
@@ -0,0 +1,5 @@
+module Surfers
+  module Log
+    VERSION = "0.1.0"
+  end
+end
