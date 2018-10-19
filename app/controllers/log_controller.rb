42ce60ff23e155a077284fd487dbd85b2c1a910a
Michael Pieschel <sixtyhectares@yahoo.com>
2018-10-19 19:05:10 +0000

    update controllers

--- app/controllers/log_controller.rb
+++ app/controllers/log_controller.rb
@@ -5,16 +5,36 @@ class LogController < ApplicationController
   enable :sessions
 
   get '/log_entries' do
-    erb :'/logs/log_entries'
+    if logged_in?
+      @logs = Logs.all.sort_by { |log| log.date }.reverse
+      erb :'/logs/log_entries'
+    else
+      please_log_in
+    end
   end
 
   get '/new' do
-    erb :'/logs/new_log_entry'
+    if logged_in?
+      erb :'/logs/new_log_entry'
+    else
+      please_log_in
+    end
   end
 
   get '/logs/:id/edit' do
-    erb :'/logs/edit_log_entry'
+    @log = Log.find_by_id(params[:id])
+    if logged_in? && current_user.logs.include?(@log)
+      erb :'/logs/edit_log_entry'
+    else
+      please_log_in
+    end
   end
 
+  helpers do
+    def please_log_in
+      flash[:message] = "You must be logged in."
+      redirect to '/login'
+    end
 
+  end
 end
