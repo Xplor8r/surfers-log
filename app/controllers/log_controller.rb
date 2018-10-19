6c756e8db1ef4f0680636f9f11e8fd96f5ba2dab
Michael Pieschel <sixtyhectares@yahoo.com>
2018-10-19 17:57:10 +0000

    views

--- app/controllers/log_controller.rb
+++ app/controllers/log_controller.rb
@@ -2,4 +2,19 @@ require 'rack-flash'
 
 class LogController < ApplicationController
   use Rack::Flash
+  enable :sessions
+
+  get '/log_entries' do
+    erb :'/logs/log_entries'
+  end
+
+  get '/new' do
+    erb :'/logs/new_log_entry'
+  end
+
+  get '/logs/:id/edit' do
+    erb :'/logs/edit_log_entry'
+  end
+
+
 end
--- app/controllers/surfer_controller.rb
+++ app/controllers/surfer_controller.rb
@@ -1,6 +1,23 @@
 require 'rack-flash'
 class SurferController < ApplicationController
   use Rack::Flash
-
   enable :sessions
+
+  get '/create_surfer' do
+    erb :'/surfers/create_surfer'
+  end
+
+  get '/login' do
+    erb :'/surfers/login'
+  end
+
+  get '/show' do
+    erb :'/surfers/show'
+  end
+
+  get '/logout' do
+    redirect to '/login'
+  end
+
+
 end
