38427bfae78e981737384a0a9d7517d4aa0c8aa8
Michael Pieschel <sixtyhectares@yahoo.com>
2018-10-21 05:06:10 +0000

    log_entries controller routes

--- app/controllers/log_entries_controller.rb
+++ app/controllers/log_entries_controller.rb
@@ -5,7 +5,7 @@ class LogEntriesController < ApplicationController
 
   get '/log_entries' do
     if logged_in?
-      @logs = LogEntry.all.sort_by { |log| log.date }.reverse
+      @log_entries = LogEntry.all.sort_by { |log| log.date }.reverse
       erb :'/logs/log_entries'
     else
       please_log_in
@@ -15,6 +15,44 @@ class LogEntriesController < ApplicationController
   get '/new' do
     if logged_in?
       erb :'/logs/new_log_entry'
+    else
+      please_log_in
+    end
+  end
+
+  post '/log_entries' do
+    if logged_in? && params['surf_spot'] != "" && params['date'] != ""  && params['content'] != ""
+      @log_entry = current_user.log_entries.create(surf_spot: params['surf_spot'], date: params['date'], content: params['content'])
+      if @log_entry
+        if params['swell_direction'] != ""
+          @log_entry.swell_direction == params['swell_direction']
+        elsif params['swell_size'] != ""
+          @log_entry.swell_size == params['swell_size']
+        elsif params['conditions'] != ""
+          @log_entry.conditions == params['conditions']
+        elsif params['swell_size'] != ""
+          @log_entry.swell_size == params['swell_size']
+        elsif params['wave_count'] != ""
+          @log_entry.wave_count == params['wave_count']
+        elsif params['image_url'] != ""
+          @log_entry.image_url == params['image_url']
+        end
+        @log_entry.save
+        redirect to "/logs/#{@log_entry.id}"
+      else
+        flash[:message] = "Please enter Surf Spot Name, Date of Session, and Log Entry Content."
+        redirect to '/logs/new_log_entry'
+      end
+    else
+      please_log_in
+    end
+    current_user.save
+  end
+
+  get '/logs/:id' do
+    if logged_in?
+      @log_entry = LogEntry.find_by_id(params[:id])
+      erb :'/logs/show_log_entry'
     else
       please_log_in
     end
